47970a55aa6808b8d8e6562da521a6d04694e5fb
hewangtong <hwt0521@126.com>
2020-06-14 22:19:00 +0800

    fix error

--- class_20200510/learn_request.py
+++ class_20200510/learn_request.py
@@ -1,10 +1,14 @@
 #coding: utf-8
 #author = hewangtong
 #date = 2020/5/10
-
 import requests
 
-url = 'http://47.107.168.87:8080/futureloan/mvc/api/member/register'
-param = {'mobilephone':'18813989009', 'pwd':'123456', 'regname':'lemonhuahua'}
-resp = requests.get(url, params=param)
-print(resp)
+#url = 'http://47.107.168.87:8080/futureloan/mvc/api/member/register'
+url = 'https://www.baidu.com/'
+#param = {'mobilephone':'18813989009', 'pwd':'123456', 'regname':'lemonhuahua'}
+
+resp = requests.get(url)
+print(resp.json())
+
+
+
